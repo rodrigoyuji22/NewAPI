cbc7b1fccb9b2b5b2c41582935df2fac8044f747
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-06-23 00:39:30 -0300

    feat: TaskItemProfile.cs

---
+++ NewAPI/Profiles/TaskItemProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NewAPI.Dtos;
+using NewAPI.Entities;
+
+namespace NewAPI.Profiles;
+
+public class TaskItemProfile : Profile
+{
+    public TaskItemProfile()
+    {
+        CreateMap<CreateTaskItemDto, TaskItem>();
+        CreateMap<TaskItem, ReadTaskItemDto>();
+        CreateMap<UpdateTaskItemDto, TaskItem>();
+    }
+}
