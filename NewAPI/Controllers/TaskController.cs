5b104e5803c07c0d03fb172d018110e750e6161f
rodrigoyuji22 <rodrigoyujik@gmail.com>
2025-06-23 21:39:53 -0300

    feat: TaskController + GetUserId method to extract the userId from user claims

--- NewAPI/Controllers/TaskController.cs
+++ NewAPI/Controllers/TaskController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NewAPI.Dtos;
+using NewAPI.Extensions;
+using NewAPI.Repositories.Interfaces;
 
 namespace NewAPI.Controllers;
 [Authorize]
 [Controller]
 [Route("[controller]")]
-public class TaskController : ControllerBase
+public class TaskController(ITaskItemService taskItemService) : ControllerBase
 {
-    
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateTaskItemAsync(CreateTaskItemDto dto)
+    {
+        var userId = User.GetUserById();
+        if (userId is null)
+            return Unauthorized();
+        var result = await taskItemService.CreateTaskItemAsync(dto, userId);
+        return Ok(result);
+    }
 }
---
+++ NewAPI/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace NewAPI.Extensions;
+
+public static class ClaimsPrincipalExtension
+{
+    public static string? GetUserById(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    }
+}
